1536878843bd47305d39c3b8d507263077c65d52
JuliValencia <juliana.valencia1@gmail.com>
2023-05-18 10:51:22 +0200

    Update Images.js

--- src/Images.js
+++ src/Images.js
@@ -10,13 +10,8 @@ export default function Images(props) {
           {props.images.map(function (image, index) {
             return (
               <div className="col-4" key={index}>
-                <a
-                  href={image.src.original}
-                  target="_blank"
-                  rel="noreferrer"
-                  alt=""
-                >
-                  <img src={image.src.landscape} className="img-fluid" />
+                <a href={image.src.original} target="_blank" rel="noreferrer">
+                  <img src={image.src.landscape} className="img-fluid" alt="" />
                 </a>
               </div>
             );
